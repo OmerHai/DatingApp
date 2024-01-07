0de73217b5b57ed25ffb0eb7f33a4d10065d132b
Omer Haimovich <omer16493@gmail.com>
2024-01-07 11:05:39 +0200

    Add entity framework to api

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace API;
+
+// Define a public class named AppUser
+public class AppUser
+{
+    // Public property representing the unique identifier of an AppUser
+    public int Id { get; set; }
+
+    // Public property representing the username of an AppUser
+    public string UserName { get; set; }
+}
