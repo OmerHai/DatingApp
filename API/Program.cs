771c061b452492cd7bc7af4e01f62b71c663b442
Omer Haimovich <omer16493@gmail.com>
2024-01-10 14:05:21 +0200

    Add cors to api

--- API/Program.cs
+++ API/Program.cs
@@ -12,13 +12,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 var app = builder.Build(); // Building the WebApplication using the configured services
 
-// Configure the HTTP request pipeline.
-app.UseHttpsRedirection(); // Redirecting HTTP requests to HTTPS
-
-app.UseAuthorization(); // Adding authorization middleware to the pipeline
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers(); // Mapping the controllers to handle incoming requests
 
 app.Run(); // Running the application
