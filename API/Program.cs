35da987ebe413e6129c9e41999d19c083592cf21
Omer Haimovich <omer16493@gmail.com>
2024-01-11 12:00:24 +0200

    Add https to angular using mkcert

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 builder.Services.AddCors();
 var app = builder.Build(); // Building the WebApplication using the configured services
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers(); // Mapping the controllers to handle incoming requests
 
 app.Run(); // Running the application
