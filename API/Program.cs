c520a3ef39119a7bd4ac0fabe177d09a494b2cf0
Omer Haimovich <omer16493@gmail.com>
2024-01-16 12:37:01 +0200

    Add a token service to api

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args); // Creating a new instance of WebApplicationBuilder
@@ -12,9 +14,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddCors();
+builder.Services.AddCors(); // Adding Cross-Origin Resource Sharing (CORS) services
+builder.Services.AddScoped<ITokenService, TokenService>(); // Adding a scoped dependency for the ITokenService interface with TokenService implementation
+
 var app = builder.Build(); // Building the WebApplication using the configured services
 
+// Configuring Cross-Origin Resource Sharing (CORS) policies
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers(); // Mapping the controllers to handle incoming requests
 
---
+++ API/interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+
+namespace API.Interfaces;
+
+// Interface for defining token-related operations
+public interface ITokenService
+{
+    // Method signature for creating a token based on an AppUser
+    string CreateToken(AppUser user);
+}
