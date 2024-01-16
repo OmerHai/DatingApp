cb51341e1f03aef61f6cc89b2a3e4e5dc66fd36e
Omer Haimovich <omer16493@gmail.com>
2024-01-16 12:04:21 +0200

    Add validation to register in api

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,11 +1,15 @@
-﻿namespace API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
 
 // Data Transfer Object (DTO) for user registration
 public class RegisterDto
 {
     // Property to store the username provided during registration
+    [Required]
     public string Username { get; set; }
 
     // Property to store the password provided during registration
+    [Required]
     public string Password { get; set; }
 }
